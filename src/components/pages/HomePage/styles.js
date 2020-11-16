038b62f5b130dd333b3ed95bb14f528a128f03c5
tatianafischer <tatiana.fischer@acad.pucrs.br>
2020-11-16 09:40:56 -0300

    componente products

--- src/components/pages/HomePage/styles.js
+++ src/components/pages/HomePage/styles.js
@@ -18,6 +18,33 @@ export const HomeHeaderContainer = styled.div`
   margin-bottom: 40px;
   border-bottom: 1px solid rgb(0,0,0,0.5);
 `
+
+export const CategorySelector = styled.select`
+  width: 50vw;
+  height: 5vh;
+  font-size: 15px;
+`
+
+export const ProductLine = styled.li`
+  display: flex;
+  align-items: center;
+  flex-direction: row;
+  justify-content: space-between;
+  flex-wrap: wrap;  
+
+  width: 70vw;  
+  padding: 10px 20px;
+  margin: 20px;
+  border-bottom: 1px solid rgb(0,0,0,0.2);
+  
+  
+`
+
+export const ProductImage = styled.img`
+  width: 125px;
+  height: 100px;
+`
+
 export const CountingCartContainer = styled.div`
   width: 60px;
   height: 50px;
