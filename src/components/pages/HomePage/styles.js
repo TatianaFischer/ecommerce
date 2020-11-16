c2b6754215039246fe4d6eb8f9a5d54b0dafed92
tatianafischer <tatiana.fischer@acad.pucrs.br>
2020-11-16 09:16:27 -0300

    estilização do filtro

--- src/components/pages/HomePage/styles.js
+++ src/components/pages/HomePage/styles.js
@@ -41,29 +41,3 @@ export const QuantityOfProducts = styled.span`
   right: 15px;
   top: 5px;
 `
-
-export const CategorySelector = styled.select`
-  width: 50vw;
-  height: 5vh;
-  font-size: 15px;
-`
-
-export const ProductLine = styled.li`
-  display: flex;
-  align-items: center;
-  flex-direction: row;
-  justify-content: space-between;
-  flex-wrap: wrap;  
-
-  width: 70vw;  
-  padding: 10px 20px;
-  margin: 20px;
-  border-bottom: 1px solid rgb(0,0,0,0.2);
-  
-  
-`
-
-export const ProductImage = styled.img`
-  width: 125px;
-  height: 100px;
-`
